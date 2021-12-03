c8958844ed66b3871e5ebf8faa3a5e3075c65476
AH <11868718+Aurinhogers@users.noreply.github.com>
2021-12-03 16:56:30 +0100

    Day one second solution

--- src/main/days/dayOne/DayOne.java
+++ src/main/days/dayOne/DayOne.java
@@ -9,17 +9,32 @@ public class DayOne {
 
     public static void main(String[] args) {
         List<Integer> inputList = FileReader.transformFileToIntegerList("dayOne.txt");
-        int increase = 0, decrease = 0;
 
-       for( int i= 0; i< inputList.size()-1 ; i++){
-        System.out.println (inputList.get(i));
-
-           if(inputList.get(i) > inputList.get(i+1)) {decrease++;}
-           else increase++;
-       }
-
-         System.out.println ("The answer is " + increase);
+         System.out.println ("The answer is " + getAmountofincreases(inputList));
+         System.out.println("The second answer is " + getAmountofincreasesWithinWindow(inputList));
         }
+
+        public static int getAmountofincreases(List<Integer>list){
+            int increase = 0, decrease = 0;
+
+            for( int i= 0; i< list.size()-1 ; i++){     
+                if(list.get(i) > list.get(i+1)) {decrease++;}
+                else increase++;
+            }
+            return increase;
+        }
+
+        public static int getAmountofincreasesWithinWindow(List<Integer>list){
+            int increase = 0;
+            for( int i= 0; i< list.size()-3 ; i++){
+    
+                if(list.get(i) < list.get(i+3)) {increase++;}
+            }
+            return increase;
+        }
+
+
+        
 
 }
 
